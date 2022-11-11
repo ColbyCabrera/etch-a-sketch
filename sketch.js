dab77ce8f1e76cf2b4edde29cd79bb81e6ee2b31
ColbyCabrera <gdthyispro@gmail.com>
2022-11-11 18:22:32 -0500

    create grid

--- sketch.js
+++ sketch.js
@@ -1,7 +1,18 @@
 
-
 function createGrid(dimensions) {
+
     
+
+    for (let i = 0; i < dimensions; i++) {
+        for (j = 0; j < dimensions; j++) {
+            let gridElement = document.createElement("div");
+            gridContainer.appendChild(gridElement);
+            gridElement.style.width = gridSize / dimensions + "rem";
+            gridElement.classList.add("grid-element");
+            gridElement.classList.add(i + "x");
+            gridElement.classList.add(j + "y");
+        }
+    }
 }
 
 function updateElement() {
@@ -11,10 +22,11 @@ function updateElement() {
 //grid size in rems where 1 rem = 10px
 const gridSize = 64;
 const gridContainer = document.createElement("div");
-let gridDimensions = 16;
+let gridDimensions = 64;
 
 document.getElementById("sketch-container").appendChild(gridContainer);
 gridContainer.style.height = gridSize + "rem";
 gridContainer.style.width = gridSize + "rem";
+gridContainer.className = "grid-container";
 
 createGrid(gridDimensions);
