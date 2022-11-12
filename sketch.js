48ea72c2834619015bb0301f320fdc66622c7e06
ColbyCabrera <gdthyispro@gmail.com>
2022-11-12 15:50:42 -0500

    add clear and create grid options

--- sketch.js
+++ sketch.js
@@ -1,18 +1,22 @@
+//grid size in rems where 1 rem = 10px
+const gridSize = 64;
+const gridContainer = document.createElement("div");
+const clearButton = document.getElementsByClassName("clear")[0];
+const create = document.getElementsByClassName("create")[0];
 
-function createGrid(dimensions) {
+document.getElementById("sketch-container").appendChild(gridContainer);
+gridContainer.style.height = gridSize + "rem";
+gridContainer.style.width = gridSize + "rem";
+gridContainer.style.minHeight = gridSize + "rem";
+gridContainer.style.minWidth = gridSize + "rem";
+gridContainer.className = "grid-container";
 
-    // creates grid elements with coordinates
-    for (let i = 0; i < dimensions; i++) {
-        for (j = 0; j < dimensions; j++) {
-            let gridElement = document.createElement("div");
-            gridContainer.appendChild(gridElement);
-            gridElement.style.width = gridSize / dimensions + "rem";
-            gridElement.classList.add("grid-element");
-            gridElement.classList.add(i + "y");
-            gridElement.classList.add(j + "x");
-        }
-    }
-}
+createGrid();
+
+addEventListener('click', updateElement);
+addEventListener('mouseover', updateElement);
+clearButton.addEventListener('click', clearGrid);
+create.addEventListener('click', createGrid);
 
 function updateElement(event) {
 
@@ -23,19 +27,42 @@ function updateElement(event) {
     }
 }
 
-//grid size in rems where 1 rem = 10px
-const gridSize = 64;
-const gridContainer = document.createElement("div");
-let gridDimensions = 16;
+function clearGrid() {
+    gridElements = Array.from(document.getElementsByClassName("grid-element"));
 
-document.getElementById("sketch-container").appendChild(gridContainer);
-gridContainer.style.height = gridSize + "rem";
-gridContainer.style.width = gridSize + "rem";
-gridContainer.style.minHeight = gridSize + "rem";
-gridContainer.style.minWidth = gridSize + "rem";
-gridContainer.className = "grid-container";
+    gridElements.forEach(element => {
+        element.style.backgroundColor = "black";
+    });
+}
 
-createGrid(gridDimensions);
+function deleteGrid() {
+    
+    let child = gridContainer.firstElementChild;
+    console.log(child);
+    while(child) {
+        gridContainer.removeChild(child);
+        child = gridContainer.firstElementChild;
+    }
+}
 
-addEventListener('click', updateElement);
-addEventListener('mouseover', updateElement);
+function createGrid() {
+
+    // creates grid elements with coordinates
+    const dimensions = document.getElementById("dimensions").value;
+    if (dimensions <= 100 && dimensions >= 1) {
+        deleteGrid();
+
+        for (let i = 0; i < dimensions; i++) {
+            for (j = 0; j < dimensions; j++) {
+                let gridElement = document.createElement("div");
+                gridContainer.appendChild(gridElement);
+                gridElement.style.width = gridSize / dimensions + "rem";
+                gridElement.classList.add("grid-element");
+                gridElement.classList.add(i + "y");
+                gridElement.classList.add(j + "x");
+            }
+        }
+    } else {
+        alert("Dimensions must be from 1 to 100");
+    }
+}
